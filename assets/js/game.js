25677633a7167a6d96b46acac40792edb7ec2390
cola-mars <86696492+cola-mars@users.noreply.github.com>
2021-12-08 18:43:29 -0500

    shop function

--- assets/js/game.js
+++ assets/js/game.js
@@ -70,6 +70,13 @@ var startGame = function () {
       var pickedEnemyName = enemyNames[i];
       enemyHealth = 50;
       fight(pickedEnemyName);
+      //if we're not at the last enemy in the array
+      if (playerHealth > 0 && i < enemyNames.length - 1) {
+        var storeConfirm = window.confirm("The fight is over, visit the store before the next round?");
+        if (storeConfirm) {
+        shop();
+        }
+      }
     } else {
       window.alert("You have lost your robot in battle! Game Over!");
     }
@@ -99,6 +106,51 @@ var endGame = function() {
   }
 };
 
+//shop function
+var shop = function () {
+  var shopOptionPrompt = window.prompt("Would you like to REFILL, your health, UPGRADE your attack, or LEAVE the store? Please enter one: 'REFILL', 'UPGRADE', or 'LEAVE' to make a choice.");
+
+  //shop option switch
+  switch(shopOptionPrompt) {
+    case "REFILL":
+    case "refill":
+      if(playerMoney >= 7){
+      window.alert("Refilling player's health by 20 for 7 dollars.");
+      //+health -money
+      playerHealth = playerHealth + 20;
+      playerMoney =playerMoney - 7;
+      } else {
+        window.alert("You don't have enough money!");
+      }
+      break;
+    
+      case "UPGRADE":
+      case "upgrade":
+      if(playerMoney >= 7) {
+      window.alert("Upgrading player's attack by 6 for 7 dollars");
+      //+attack - money
+      playerAttack = playerAttack + 6;
+      playerMoney = playerMoney -7;
+      } else {
+        window.alert("You don't have enough money!");
+      }
+      break;
+
+      case "LEAVE":
+      case "leave":
+      window.alert("Leaving the store.");
+      //do nothing so function ends
+      break;
+    
+    default: 
+      window.alert("You did not pick a valid option. Try again.");
+
+    //call shop() again to force player to pick a valid option
+    shop();
+    break;
+  }
+}
+
+
 //page load game starts
-debugger;
 startGame();
